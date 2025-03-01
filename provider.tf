d956dd68e067c3607b2ee1d503fd78518ed3cf54
YashNetOps998 <navadiya.ya@northeastern.edu>
2025-03-01 14:40:41 -0500

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,7 +1,7 @@
 provider "aviatrix" {
   controller_ip = var.controller_ip
- # username      = var.username
- # password      = var.password
+ username      = var.username
+ password      = var.password
 }
 
 provider "aws" {
