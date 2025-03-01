c66cf8601cf323e8119fdb984fb0c666dc1a6fd7
YashNetOps998 <navadiya.ya@northeastern.edu>
2025-03-01 16:23:30 -0500

    Update versions.tf

--- versions.tf
+++ versions.tf
@@ -3,7 +3,7 @@ terraform {
   required_providers {
     aviatrix = {
       source  = "AviatrixSystems/aviatrix"
-      version = "~> 3.0.0"
+      version = "~> 2.24.4"
     }
     aws = {
       source  = "hashicorp/aws"
